cfc8f065b77174ff2fccecb21cd367366fedcb69
Alexander <1alexvash@gmail.com>
2020-03-16 19:14:51 +0200

    Updates

---
+++ precache-manifest.d44c90fb6f3cc413f3dd8ed29d65f1c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3436054d44d1036bd24fd610f90d6629",
+    "url": "/RandomMovie/index.html"
+  },
+  {
+    "revision": "d7b4dd8e2498fe70dc90",
+    "url": "/RandomMovie/static/css/main.d390dfcf.chunk.css"
+  },
+  {
+    "revision": "951c4c325313cb9cdd61",
+    "url": "/RandomMovie/static/js/2.eafe8ee1.chunk.js"
+  },
+  {
+    "revision": "d7b4dd8e2498fe70dc90",
+    "url": "/RandomMovie/static/js/main.0d8e0a93.chunk.js"
+  },
+  {
+    "revision": "6b52ea7bb5dfc86cced3",
+    "url": "/RandomMovie/static/js/runtime~main.c79c5349.js"
+  },
+  {
+    "revision": "b51720275aea4ffaf77d94180e482e73",
+    "url": "/RandomMovie/static/media/google.b5172027.png"
+  },
+  {
+    "revision": "6abe06c27baf88fa1f86fc4775c518c5",
+    "url": "/RandomMovie/static/media/preloader-bg.6abe06c2.jpg"
+  },
+  {
+    "revision": "d2b2e36e1c7f0808a44a554daa4d365a",
+    "url": "/RandomMovie/static/media/star.d2b2e36e.jpg"
+  },
+  {
+    "revision": "87f579ec1d4fbd0cea26e64cbd90b808",
+    "url": "/RandomMovie/static/media/torrent.87f579ec.png"
+  },
+  {
+    "revision": "5378d37ab8520cdce325b4b29c9931e9",
+    "url": "/RandomMovie/static/media/youtube.5378d37a.png"
+  }
+]);
